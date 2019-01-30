67e6076d4f50956f29833079f582ddbdeb5a5c3b
Loric Brevet <loric.brevet@gmail.com>
2019-01-30 20:42:49 +0100

    i3 use rofi for workspace renaming

--- graphical/.config/i3/rename_workspace.sh
+++ graphical/.config/i3/rename_workspace.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 set -e
 
-prompt="New name: "
+hist=$HOME/.i3_rename_history
+
+# current workspace num
 num=`i3-msg -t get_workspaces | jq -r 'map(select(.focused == true))[0].num'`
-old_name=`i3-msg -t get_workspaces | jq -r 'map(select(.focused == true))[0].name'`
 
-# Do the renaming
-i3-input -F "rename workspace to \"$num:%s\"" -P "$prompt"
-
-# Reset to num if input empty
-name=`i3-msg -t get_workspaces | jq -r 'map(select(.focused == true))[0].name'`
-if [[ ${name%"$prefix"} =~ ^[0-9]+:$ ]]; then
-  i3-msg "rename workspace to \"$num\""
+# create history file if first time
+if [ ! -f $hist ]; then
+    touch $hist
 fi
+
+# prompt for name
+input=$( cat $hist | rofi -dmenu -p "New name")
+
+# rename
+if [ -z "$input" ]
+then
+    i3-msg "rename workspace to \"$num\""
+else
+    i3-msg "rename workspace to \"$num:$input\""
+fi
+
+# add to history
+echo $input >> $hist
