b57df1b7013ea961e3e34b4fcc246580fb7abe01
Loric Brevet <loric.brevet@gmail.com>
2019-02-20 14:16:25 +0100

    Rename script and set lightdm

--- packer/scripts/10-setup.sh
+++ packer/scripts/10-setup.sh
@@ -9,9 +9,5 @@ apt-get install -y sudo
 echo "%sudo ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 usermod -aG sudo dev
 
-# Enable autologin
-sed -i "s/#  AutomaticLoginEnable = true/AutomaticLoginEnable = true/g" /etc/gdm3/custom.conf && \
-sed -i "s/#  AutomaticLogin = user1/AutomaticLogin = dev/g" /etc/gdm3/custom.conf
-
 # Decrease GRUB wait time
 echo "GRUB_RECORDFAIL_TIMEOUT=5" | tee -a /etc/default/grub > /dev/null && update-grub
---
+++ packer/scripts/30-provision.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set zsh as default shell
+sudo chsh -s /bin/zsh dev
+
+# Run dotfiles
+rm -rf $HOME/.config/dotfiles
+git clone https://github.com/loric-/dotfiles.git $HOME/.config/dotfiles
+cd $HOME/.config/dotfiles && python3 link.py
+
+# Install vim plugins
+vim +'PlugInstall --sync' +qa
+
+# Apply colors
+xrdb $HOME/.Xresources
+
+# Install lightdm instead of gdm
+sudo apt-get remove -y gdm3
+sudo apt-get install -y lightdm
+
+# Set i3 as default session
+echo "[SeatDefaults]"           | sudo tee -a /usr/share/lightdm/lightdm.conf.d/60-autologin-vagrant.conf
+echo "autologin-user=dev"       | sudo tee -a /usr/share/lightdm/lightdm.conf.d/60-autologin-vagrant.conf
+echo "autologin-user-timeout=0" | sudo tee -a /usr/share/lightdm/lightdm.conf.d/60-autologin-vagrant.conf
+echo "user-session=i3"          | sudo tee -a /usr/share/lightdm/lightdm.conf.d/60-autologin-vagrant.conf
