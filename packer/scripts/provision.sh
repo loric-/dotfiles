d1bbf29de5ef00698564984d3a2e54c63a0246e1
Loric Brevet <loric.brevet@gmail.com>
2019-02-20 10:31:20 +0100

    Set i3 as default packer

--- packer/scripts/provision.sh
+++ packer/scripts/provision.sh
@@ -10,3 +10,6 @@ cd $HOME/.config/dotfiles && python3 link.py
 
 # Install vim plugins
 vim -c 'PlugInstall' -c 'qa!' > /dev/null
+
+# Set i3 as default session
+sudo sed -i "/\[daemon\]/a DefaultSession=i3" /etc/gdm3/custom.conf
