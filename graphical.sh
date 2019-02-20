d39de550e6a9635ac8239fb1684a39d996f3c4b1
Loric Brevet <loric.brevet@gmail.com>
2019-02-20 09:31:33 +0100

    Update graphical script

--- graphical.sh
+++ graphical.sh
@@ -6,36 +6,27 @@ sudo apt-get update
 
 # Install i3 gaps dependencies
 sudo apt-get install -y \
-    libxcb1-dev \
-    libxcb-keysyms1-dev \
-    libpango1.0-dev \
-    libxcb-util0-dev \
-    libxcb-icccm4-dev \
-    libyajl-dev \
-    libstartup-notification0-dev \
-    libxcb-randr0-dev \
-    libev-dev \
-    libxcb-cursor-dev \
-    libxcb-xinerama0-dev \
-    libxcb-xkb-dev \
-    libxkbcommon-dev \
-    libxkbcommon-x11-dev \
-    autoconf \
-    xutils-dev \
-    libtool \
-    libxcb-shape0-dev
-
-sudo add-apt-repository -y ppa:aguignard/ppa
-sudo apt-get update
-sudo apt-get install -y libxcb-xrm-dev
+    libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
+    libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
+    libstartup-notification0-dev libxcb-randr0-dev \
+    libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+    libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
+    autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
 
 # Install i3
-rm -rf i3-gaps
+cd $HOME/Lab
+
+# clone the repository
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
+
+# compile & install
 autoreconf --force --install
 rm -rf build/
 mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
 ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
 make
 sudo make install
@@ -178,7 +169,6 @@ sudo apt-get install -y \
     filezilla \
     pinta \
     shutter \
-    mysql-workbench \
     gnome-control-center
 
 # Install apps
