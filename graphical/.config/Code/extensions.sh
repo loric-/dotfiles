cfef1b0fc2d0cec51d3558bc86740c7799dd1d75
Loric Brevet <loric.brevet@amersports.com>
2019-05-09 11:12:08 +0200

    Set meld again as default for git and update vscode extensions

--- graphical/.config/Code/extensions.sh
+++ graphical/.config/Code/extensions.sh
@@ -1,6 +1,6 @@
 code --install-extension alefragnani.project-manager
 code --install-extension Equinusocio.vsc-material-theme
+code --install-extension fayras.simple-new-file
 code --install-extension ms-vscode.Go
-code --install-extension patbenatar.advanced-new-file
 code --install-extension PKief.material-icon-theme
 code --install-extension vscodevim.vim
