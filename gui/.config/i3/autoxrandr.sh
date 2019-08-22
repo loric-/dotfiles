74c1ec242dfc9e9019be3c5027139126938c3816
Loric Brevet <loric.brevet@amersports.com>
2019-08-22 11:37:20 +0200

    Script to set monitors with crtc failing errors handled

--- gui/.config/i3/autoxrandr.sh
+++ gui/.config/i3/autoxrandr.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# Get CRTC information
+max_crtc=$(xrandr --listproviders | grep -oP 'crtcs:\s*\K\d+')
+
+# $1 is output name
+# $2 is the previous monitor
+# if no previous monitor, the output is set as primary
+# if xrandr fails, try a different crtc
+function xrandrEnable() {
+    local output=$1
+
+    local previous=""
+    local primary="--primary"
+    if [[ -n $2 ]]; then
+        previous="--right-of $2"
+        primary=""
+    fi
+
+    # Try to set monitor with auto crtc
+    xrandr --output $output --pos 0x0 --auto $primary $previous
+
+    # If did not work, try manually setting the crtc
+    if [ "$?" -ne 0 ]; then
+        for crtc in $(seq 0 $((max_crtc - 1))); do
+            xrandr --output $output --crtc $crtc --pos 0x0 --auto $primary $previous && break
+        done
+    fi
+}
+
 # Get connected outputs
 connected_outputs=$(xrandr | grep '\Wconnected' | awk '{ print $1 }')
 connected_outputs=($connected_outputs)
@@ -9,29 +37,31 @@ connected_outputs_count=${#connected_outputs[@]}
 disconnected_outputs=$(xrandr | grep '\Wdisconnected' | awk '{ print $1 }')
 disconnected_outputs=($disconnected_outputs)
 
+# Turn off disconnected displays
+for output in ${disconnected_outputs[@]}; do
+    xrandr --output $output --off
+done
+
+# Turn off connected displays to reset them
+# before enabling them again
+for output in ${connected_outputs[@]}; do
+    xrandr --output $output --off
+done
+
 # Get internal monitor (first monitor)
 internal=${connected_outputs[0]}
 only_internal=1
 
 # Update internal
-xrandr --output $internal --auto --primary
-if [ $connected_outputs_count -eq 1 ]; then
-    notify-send "Monitor Update: only internal display" 
-fi
+xrandrEnable $internal
 
 # Update additional displays
 previous=$internal
 for output in ${connected_outputs[@]}; do
     if [[ ! $output =~ ^$internal.*$ ]]; then
-       xrandr --output $output --pos 0x0 --auto --right-of $previous
-       previous=$output
-       notify-send "Monitor Update: $output plugged in"
+        xrandrEnable $output $previous
+        previous=$output
     fi
-done
-
-# Update disconnected displays
-for output in ${disconnected_outputs[@]}; do
-    xrandr --output $output --off
 done
 
 # Reload i3
--- gui/.config/polybar/launch.sh
+++ gui/.config/polybar/launch.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
@@ -12,6 +12,6 @@ if type "xrandr"; then
     MONITOR=$m polybar --reload bottom &
   done
 else
-  polybar --reload top &
-  polybar --reload bottom &
+    polybar --reload top &
+    polybar --reload bottom &
 fi
