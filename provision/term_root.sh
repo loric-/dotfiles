1365feb2ca2018757466a964a46c408187cd90ce
Loric Brevet <loric.brevet@amersports.com>
2019-09-12 14:06:38 +0200

    Change docker gid in iso image

--- provision/term_root.sh
+++ provision/term_root.sh
@@ -100,3 +100,7 @@ apt-get install -y docker-ce
 # Install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Change docker group GID
+groupmod -g 990 docker
+find / -group 999 -xdev -exec chgrp -h docker {} \;
